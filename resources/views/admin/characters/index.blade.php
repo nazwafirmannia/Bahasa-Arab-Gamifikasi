b61bde8778bcf4056420d760067714ad5e742efc
nazwafirmannia <nazwafirmannia@gmail.com>
2026-07-09 18:03:38 +0700

    perbaiki variabel char di admin crud

--- resources/views/admin/characters/index.blade.php
+++ resources/views/admin/characters/index.blade.php
@@ -145,7 +145,7 @@
     <div class="avatar-grid">
         @forelse($characters as $char)
         @php
-            $imgSrc = asset('images/characters/' . $schar->level . '.jpg'); 
+            $imgSrc = asset('images/characters/' . ($char->level ?? 1) . '.jpg'); 
         @endphp
 
         <div class="avatar-card {{ $char->is_active ? 'avatar-card--active' : 'avatar-card--inactive' }}">
