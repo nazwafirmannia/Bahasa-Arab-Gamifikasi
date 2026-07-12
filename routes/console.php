4083657194e18e757561ff6ca16311db3f01296c
nazwafirmannia <nazwafirmannia@gmail.com>
2026-07-13 01:49:54 +0700

    nonaktif reminder learning

--- routes/console.php
+++ routes/console.php
@@ -8,5 +8,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('reminder:study')
-        ->everyMinute();
+//Schedule::command('reminder:study')
+    //->everyMinute();
